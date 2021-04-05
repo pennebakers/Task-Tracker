c7a6f2f71777d0a7e24fb8caccc87d5ce06f4bf9
Seth Pennebaker <48425931+pennebakers@users.noreply.github.com>
2021-04-05 16:31:09 -0400

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const { default: Button } = require("./Button")
+
+const Header = ({ onAdd, showAdd }) => {
+
+    return (
+        <header className='header'>
+            <h1>Task Tracker</h1>
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Tasks.js
@@ -0,0 +1,17 @@
+import Task from './Task.js'
+
+const Tasks = ({ tasks, onDelete, onToggle}) => {
+    return (
+        <>
+            {tasks.map((task, index) => 
+                <Task 
+                    key={index} 
+                    task={task} 
+                    onDelete={onDelete} 
+                    onToggle={onToggle}/>
+            )}
+        </>
+    )
+}
+
+export default Tasks
